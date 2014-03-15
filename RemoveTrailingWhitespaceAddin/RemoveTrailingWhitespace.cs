ec73135f33cda558cd11bbe5aaf4a66d6efe053c
Mike <mike@prime31.com>
2014-03-14 17:50:17 -0700

    initial commit

---
+++ RemoveTrailingWhitespaceAddin/RemoveTrailingWhitespace.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Core;
+using MonoDevelop.Ide;
+using Mono.TextEditor;
+using MonoDevelop.Ide.Commands;
+using MonoDevelop.Projects;
+using MonoDevelop.Projects.Extensions;
+using MonoDevelop.Ide.Gui.Components;
+using System.Windows.Input;
+using Mono.Addins;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Prime31
+{
+	public static class FileHelper
+	{
+		public static void removeTrailingWhitespace( TextEditorData editor )
+		{
+			var lines = new List<string>();
+
+			foreach( var line in editor.Lines )
+			{
+				var lineText = editor.Text.Substring( line.Offset, line.Length );
+				lines.Add( lineText.TrimEnd() );
+			}
+
+			editor.Text = string.Join( "\n", lines );
+		}
+	}
+
+
+	public class Save : CommandHandler
+	{
+		protected override void Run()
+		{
+			FileHelper.removeTrailingWhitespace( IdeApp.Workbench.ActiveDocument.Editor );
+			IdeApp.Workbench.ActiveDocument.Save();
+		}
+
+
+		protected override void Update( CommandInfo info )
+		{
+			info.Enabled = IdeApp.Workbench.ActiveDocument != null && IdeApp.Workbench.ActiveDocument.IsDirty;
+		}
+	}
+
+
+	public class SaveAll : CommandHandler
+	{
+		protected override void Run()
+		{
+			for( int i = 0; i < IdeApp.Workbench.Documents.Count; i++ )
+			{
+				if( IdeApp.Workbench.Documents[i].IsDirty )
+					FileHelper.removeTrailingWhitespace( IdeApp.Workbench.Documents[i].Editor );
+			}
+
+			IdeApp.Workbench.SaveAll();
+		}
+
+
+		protected override void Update( CommandInfo info )
+		{
+			bool hasdirty = false;
+			for( int i = 0; i < IdeApp.Workbench.Documents.Count; i++ )
+				hasdirty |= IdeApp.Workbench.Documents[i].IsDirty;
+
+			info.Enabled = hasdirty;
+		}
+	}
+
+
+	public enum BetterFileCommands
+	{
+		Save,
+		SaveAll
+	}
+
+}
+
